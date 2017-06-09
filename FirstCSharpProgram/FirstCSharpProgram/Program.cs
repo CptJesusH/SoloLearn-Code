12ed312713506a234992c42513ad4737ae4701b3
CptJesusH <lmichael1221@gmail.com>
2017-06-08 22:27:49 -0500

    Coded factorial calculator.

--- FirstCSharpProgram/FirstCSharpProgram/Program.cs
+++ FirstCSharpProgram/FirstCSharpProgram/Program.cs
@@ -10,6 +10,23 @@ namespace FirstCSharpProgram
     {
         static void Main(string[] args)
         {
+            // Displaying data on the screen.
+            Console.WriteLine("Hello world!"); // Why is "Hello World" a thing?
+
+            // Using variable placeholders to create formatted strings:
+            int x = 10;
+            double y = 20;
+            Console.WriteLine("x = {0}; y = {1}", x, y); // Output: x = 10; y = 20
+
+            // User Input
+            Console.WriteLine("What is your name?");
+            string yourName = Console.ReadLine(); // Assume Mike
+            Console.WriteLine("Hello, {0}.", yourName); // Output: Hello, Mike.
+
+            // Converting User Input to Different Data Types
+            Console.WriteLine("How old are you?");
+            int age = Convert.ToInt32(Console.ReadLine()); // Assume 25
+            Console.WriteLine("You are {0} years old.", age); // Output: You are 25 years old.
 
         }
     }
---
+++ Recursion/Recursion/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursion
+{
+    class Program
+    {
+        static double Fact(double num)
+        {
+            if(num == 1.0)
+            {
+                return 1;
+            }
+            return num * Fact(num - 1.0);
+        }
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                Console.WriteLine("Enter a number to calculate its factorial.");
+                double userInput = Convert.ToInt64(Console.ReadLine());
+                Console.WriteLine(Fact(userInput));
+            }
+        }
+    }
+}
